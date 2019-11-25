066758b1da2120a80e2c20fccd93af88b314b5b2
Tamas Horvath <horvathtamas22@gmail.com>
2019-11-25 17:42:12 +0100

    start script modified

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
 
+console.log(__dirname)
 app.use(express.static(__dirname + '/dist/'));
 app.get(/.*/, function (req, res) {
     res.sendFile(__dirname + '/dist/index.html');
